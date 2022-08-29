ed49255dc0e65e64387fda25072529c32259e07a
anonymousvegan <majjunjulavgust@gmail.com>
2022-08-29 21:44:58 +0200

    database connection

---
+++ create_database.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE learning;
+
+USE learning;
---
+++ database.php
@@ -0,0 +1,8 @@
+<?php
+
+$hostName = "localhost";
+$userName = "nikola";
+$password = "learning";
+$dbName = "learning";
+$conn = new mysqli($hostName,$userName,$password,$dbName) or die("Database error");
+
