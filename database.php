4fd0753decb111b01a68ebb4dc070990f37ec9ff
anonymousvegan <majjunjulavgust@gmail.com>
2022-08-30 15:19:28 +0200

    Finaly works!!!!!!

--- database.php
+++ database.php
@@ -1,6 +1,6 @@
 <?php
     $hostName = "localhost";
     $userName = "nikola";
-    $password = "learning";
+    $password = "nikola";
     $dbName = "learning";
     $conn = new mysqli($hostName,$userName,$password,$dbName) or die("Database error");
--- send.php
+++ send.php
@@ -1,4 +1,9 @@
 <?php
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
+
     require("database.php");
     echo "Database conncected!";
     $name = $_POST["name"];
@@ -6,7 +11,8 @@
     $age = $_POST["age"];
     $email = $_POST["email"];
     $yes = $_POST["yes"];
-    echo $name . $surname . $age . $email . $yes;
-    $stmt = $conn->prepare("INSERT INTO users VALUES (?, ?, ?, ?, ?)");
-    $stmt->bind_param($name, $surname, $age, $email, $yes);
+    $query = "INSERT into users (first_name, last_name, age, yes, Email) VALUES (?,?,?,?,?)";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param('sssds', $name, $surname, $age, $yes, $email );
     $stmt->execute();
+    $result = $stmt->get_result();    
